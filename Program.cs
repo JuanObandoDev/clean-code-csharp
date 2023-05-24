886146ab7f414ce2147ab442de1d85ebd12fc812
Juan Obando <juanobando.developer@gmail.com>
2023-05-24 18:02:06 -0500

    refactor: remove useless comments

--- Program.cs
+++ Program.cs
@@ -1,32 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ToDo {
-    internal class Program {
+namespace ToDo
+{
+    internal class Program
+    {
         public static List<string> todoList { get; set; }
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             todoList = new List<string>();
             int option = 0;
-            do {
+            do
+            {
                 option = ShowMainMenu();
-                if ((Menu)option == Menu.AddTodo) {
+                if ((Menu)option == Menu.AddTodo)
+                {
                     addTodo();
-                } else if ((Menu)option == Menu.RemoveTodo) {
+                }
+                else if ((Menu)option == Menu.RemoveTodo)
+                {
                     removeTodo();
-                } else if ((Menu)option == Menu.ShowTodoList) {
+                }
+                else if ((Menu)option == Menu.ShowTodoList)
+                {
                     showTodoList();
-                } else if ((Menu)option == Menu.Exit) {
+                }
+                else if ((Menu)option == Menu.Exit)
+                {
                     Console.WriteLine("Gracias por usar la aplicación");
-                } else {
+                }
+                else
+                {
                     Console.WriteLine("Opción no válida");
                 }
             } while ((Menu)option != Menu.Exit);
         }
-        /// <summary>
-        /// Show the main menu 
-        /// </summary>
-        /// <returns>Returns option indicated by user</returns>
-        public static int ShowMainMenu() {
+
+        public static int ShowMainMenu()
+        {
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("Ingrese la opción a realizar: ");
             Console.WriteLine("1. Nueva tarea");
@@ -39,19 +50,25 @@ namespace ToDo {
             return Convert.ToInt32(line);
         }
 
-        public static void addTodo() {
-            try {
+        public static void addTodo()
+        {
+            try
+            {
                 Console.WriteLine("Ingrese el nombre de la tarea: ");
                 string newTodo = Console.ReadLine();
                 todoList.Add(newTodo);
                 Console.WriteLine("Tarea registrada");
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 throw new Exception("Error al ingresar la tarea");
             }
         }
 
-        public static void removeTodo() {
-            try {
+        public static void removeTodo()
+        {
+            try
+            {
                 Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 // Show current taks
                 showTodoList();
@@ -59,35 +76,48 @@ namespace ToDo {
                 string todoToRemove = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(todoToRemove) - 1;
-                if(indexToRemove > (todoList.Count - 1) || indexToRemove < 0) {
+                if (indexToRemove > (todoList.Count - 1) || indexToRemove < 0)
+                {
                     Console.WriteLine("Tarea no encontrada");
-                } else {
-                    if (todoList.Count > 0 && indexToRemove > -1) {
+                }
+                else
+                {
+                    if (todoList.Count > 0 && indexToRemove > -1)
+                    {
                         string removedTodo = todoList[indexToRemove];
                         todoList.RemoveAt(indexToRemove);
                         Console.WriteLine("Tarea " + removedTodo + " eliminada");
-                    } else {
+                    }
+                    else
+                    {
                         Console.WriteLine("No hay tareas por eliminar");
                     }
                 }
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 throw new Exception("Error al remover la tarea");
             }
         }
 
-        public static void showTodoList() {
-            if (todoList == null || todoList.Count == 0) {
+        public static void showTodoList()
+        {
+            if (todoList == null || todoList.Count == 0)
+            {
                 Console.WriteLine("No hay tareas por realizar");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("----------------------------------------");
                 int todoIndex = 0;
-                todoList.ForEach(todo=>Console.WriteLine((++todoIndex) + ". " + todo));
+                todoList.ForEach(todo => Console.WriteLine((++todoIndex) + ". " + todo));
                 Console.WriteLine("----------------------------------------");
             }
         }
     }
 
-    public enum Menu {
+    public enum Menu
+    {
         AddTodo = 1,
         RemoveTodo = 2,
         ShowTodoList = 3,
